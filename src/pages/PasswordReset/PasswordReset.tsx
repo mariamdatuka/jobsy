0ab3518448bee63a424e424348aa49f4dfcba58e
mariamdatuka <mariam.datukishvili.7@gmail.com>
2026-02-16 22:52:22 +0400

    remove unesed imports

--- src/pages/PasswordReset/PasswordReset.tsx
+++ src/pages/PasswordReset/PasswordReset.tsx
@@ -12,7 +12,7 @@ import { useNavigate } from "react-router";
 
 import { supabase } from "@src/supabase-client";
 import { PASSWORD_RESET_SUCCESS_MODAL } from "@src/modals/modal_names";
-import NiceModal, { hide } from "@ebay/nice-modal-react";
+import NiceModal from "@ebay/nice-modal-react";
 
 const PasswordReset = () => {
   const navigate = useNavigate();
@@ -50,31 +50,28 @@ const PasswordReset = () => {
   };
 
   return (
-    <>
-      <Stack height="100vh" alignItems="center" gap={2} justifyContent="center">
-        <Text variant="h6">Please enter new password</Text>
-        <FormProvider {...methods}>
-          <form onSubmit={methods.handleSubmit(onSubmit)}>
-            <Stack alignItems="center" gap={2} justifyContent="center">
-              <Input label="Password" name="password" type="password" />
-              <Input
-                label="Repeat Password"
-                name="repeatPassword"
-                type="password"
-              />
-              <MainButton
-                title="Reset Password"
-                disabled={isPending}
-                type="submit"
-                loading={isPending}
-                loadingPosition="start"
-              />
-            </Stack>
-          </form>
-        </FormProvider>
-      </Stack>
-      )
-    </>
+    <Stack height="100vh" alignItems="center" gap={2} justifyContent="center">
+      <Text variant="h6">Please enter new password</Text>
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit(onSubmit)}>
+          <Stack alignItems="center" gap={2} justifyContent="center">
+            <Input label="Password" name="password" type="password" />
+            <Input
+              label="Repeat Password"
+              name="repeatPassword"
+              type="password"
+            />
+            <MainButton
+              title="Reset Password"
+              disabled={isPending}
+              type="submit"
+              loading={isPending}
+              loadingPosition="start"
+            />
+          </Stack>
+        </form>
+      </FormProvider>
+    </Stack>
   );
 };
 
